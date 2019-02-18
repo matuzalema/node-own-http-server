85937442843db12950e21994ad4c8aece8f81b01
Jadwiga Bochinska <matuzalema@gmail.com>
2019-02-18 14:44:06 +0100

    Initialize project

---
+++ index.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var http = require ('http');
+var server = http.createServer();
+
+fs.readFile('./index.html', 'utf-8', function(err, data){
+	server.on('request', function(request, response){
+		response.setHeader("Content-Type", "text/html; charset=utf-8");
+		if(request.method === 'GET' && request.url === '/') {
+			response.write(data);
+			response.end();
+		} else {
+			response.statusCode = 404;
+			response.write("<h1>404: Zła ścieżka!</h1>");
+			response.end();
+		}
+	});
+});
+ server.listen(8080);
